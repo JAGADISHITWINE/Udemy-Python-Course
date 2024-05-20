48fd13e5d09b10d349fdd4109f1033a4c9049c54
jagadish <jagadish.s.agrahara1999@gmail.com>
2024-05-20 17:29:43 +0530

    basic of python

---
+++ Basics.py
@@ -0,0 +1,51 @@
+## Accepting User Input
+Name = input("Enter your name: ")
+print(Name)
+
+## Concatenation Of Strings
+Message = 'Hello ' + 'World'
+print(Message)
+
+## Data Types In Python
+print(type(8))
+print(type('8'))
+print(type(8.0))
+print(type(True))
+print(type(8+8j))
+
+## Variable Declaration & Assignment
+price = 100
+print(price)
+
+## Naming Conventions To Follow While Creating Variables
+radius=10
+RADIUS=10
+RADIUS=10
+rad_ius=10
+_radius=10
+radius1=10
+
+## Operators In Python
+a=10
+b=20
+print(a==b)
+b=10
+print(a==b)
+print(a!=b)
+b=20
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+## Logical Operators
+a=10
+b=10
+print(a==b)
+b = 20
+print(a==10 and b==20)
+b=30
+print(a==10 and b==20)
+print(a==10 or b==20)
+
