f9253b300997d7a5cc9e77c4809142a768490ad2
jagadish <jagadish.s.agrahara1999@gmail.com>
2024-05-29 17:28:56 +0530

    deleted

--- Functions/EMI_Calculator.py
+++
@@ -1,8 +0,0 @@
-# EMI calculator formula: P x R x (1+R)^N / [(1+R)^N-1]
-
-def emi_calculator(principal,rate,time):
-    r = rate/12/100
-    emi = (principal * r * (1+r)**time)/ ((1+r)**time-1)
-    return emi 
-
-print(emi_calculator(50000,8.5,96))
